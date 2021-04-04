6d9eac6a95176e88791e62bc2a786fe01e93fd5c
fary <fbakhsheshi@ymail.com>
2021-04-05 00:42:10 +0200

    foodor

--- lib/myBloc/myBloc.dart
+++ lib/myBloc/myBloc.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 class MyBloc extends ChangeNotifier {
   String _location = '';
   get location => _location;
-  void setLocation(String value) async {
+  void setLocation(String value) {
     _location = value;
   }
 }
