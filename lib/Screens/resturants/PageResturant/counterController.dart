662c0d173ae1370d35ba79f192ecdef3c266ba61
fary <fbakhsheshi@ymail.com>
2021-06-25 01:37:06 +0200

    getX Controller for Shopping Cart

---
+++ lib/Screens/resturants/PageResturant/counterController.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+
+class CounterController extends GetxController {
+
+  RxInt itemCount = 1.obs;
+  final counter = 1.obs;
+  RxBool showBottomSheet = false.obs;
+
+  increment() {
+    counter.value++;
+    itemCount.value = itemCount.value + counter.value;
+  }
+
+  decrement() {
+    counter.value--;
+     itemCount.value = itemCount.value - counter.value;
+
+  }
+
+  clear() {
+    itemCount.value = (counter.value * 0) + 1;
+  }
+}
