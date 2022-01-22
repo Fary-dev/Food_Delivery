cbfdac6a0e579fb24b48f69ef85156412f88ed76
fary <fbakhsheshi@ymail.com>
2022-01-22 04:47:59 +0100

    new upgrade

--- lib/Screens/resturants/Resturant_List/resturans_liste.dart
+++ lib/Screens/resturants/Resturant_List/resturans_liste.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mjam/Screens/Resturants/Resturant_List/Resturant_List_Controller.dart';
